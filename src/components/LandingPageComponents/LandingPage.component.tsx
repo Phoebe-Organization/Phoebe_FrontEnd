4b446bf9d01e870b99e57d5ab49cfe7f06e30c37
Marquis <whitley.marquis1@yahoo.com>
2022-12-06 22:26:57 -0600

    Landing Page Iphone Image, Download from App Store Button

--- src/components/LandingPageComponents/LandingPage.component.tsx
+++ src/components/LandingPageComponents/LandingPage.component.tsx
@@ -1,6 +1,20 @@
 import React, { Dispatch, SetStateAction, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getAuthToken } from '../../utils/getAuthToken';
+// import iPhone14 from '../../assets/iphone14.svg';
+import iPhone from '../../assets/iPhoneImage.png';
+import IPhoneBackgroundImg from '../../assets/pexels-rodnae-productions-8217860.jpg';
+import {
+  AppStoreButtonContainer,
+  FloatingShadow,
+  IPhoneBackgroundImage,
+  IPhoneImage,
+  IPhoneImageContainer,
+} from '../styles/landingpage';
+import FloatingImg from '../../assets/floatingShadow.png';
+import { FaApple } from 'react-icons/fa';
+// import iPhoneImage
+// import { iPhoneImage} from '../styles/landingpage'
 
 interface LandingPageProps {
   showNav: Dispatch<SetStateAction<boolean>>;
@@ -16,7 +30,24 @@ const LandingPage = () => {
     }
   }, []);
 
-  return <div>Welcome from Landing Pagfdsafdsae!</div>;
+  return (
+    <>
+      <div>Welcome from Landing Pagfdsafdsae!</div>
+      <IPhoneImageContainer>
+        <IPhoneBackgroundImage>
+          <IPhoneImage src={iPhone} />
+        </IPhoneBackgroundImage>
+        <FloatingShadow src={FloatingImg} />
+      </IPhoneImageContainer>
+      <AppStoreButtonContainer>
+        <FaApple />
+        <p>
+          Download on the <br />
+          <span className='bigTxt'>App Store</span>
+        </p>
+      </AppStoreButtonContainer>
+    </>
+  );
 };
 
 export default LandingPage;
--- src/components/styles/landingpage.tsx
+++ src/components/styles/landingpage.tsx
@@ -1,6 +1,115 @@
 import styled from 'styled-components';
 
 export const IPhoneImage = styled.img({
-  width: '150px',
+  width: '250px',
   height: '500px',
+
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  objectFit: 'cover',
+  overflow: 'hidden',
+});
+
+export const FloatingShadow = styled.img({
+  width: '250px',
+});
+
+export const IPhoneImageContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const AppStoreButtonContainer = styled.div({
+  cursor: 'pointer',
+  backgroundColor: '#000000',
+  color: 'white',
+  borderRadius: '5px',
+  width: '130px',
+  height: '41px',
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+  '.bigTxt': {
+    textTransform: 'capitalize',
+    fontSize: '14px',
+  },
+  p: {
+    textTransform: 'uppercase',
+    fontSize: '7px',
+    lineHeight: '13px',
+    margin: '0px',
+    textAlign: 'left',
+  },
+  svg: {
+    fontSize: '24px',
+  },
+});
+
+// .flex {
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//    }
+
+//    .app {
+//     &-btn {
+//      width: 45%;
+//      max-width: 160px;
+//      color: #fff;
+//      margin: 20px 10px;
+//      text-align: left;
+//      border-radius: 5px;
+//      text-decoration: none;
+//      font-family: "Lucida Grande", sans-serif;
+//      font-size: 10px;
+//      text-transform: uppercase;
+//      &.blu {
+//       background-color: #101010;
+//       transition: background-color 0.25s linear;
+//       &:hover {
+//        background-color: #454545;
+//       }
+//      }
+//      i {
+//       width: 20%;
+//       text-align: center;
+//       font-size: 28px;
+//       margin-right: 7px;
+//      }
+//      .big-txt {
+//       font-size: 17px;
+//       text-transform: capitalize;
+//      }
+//     }
+//    }
+
+// export const BackgroundImg = styled.img({
+//   position: 'absolute',
+//   top: '0',
+//   left: '0',
+//   height: '100%',
+//   width: '100%',
+//   backgroundSize: 'cover',
+//   backgroundPosition: 'center',
+//   objectFit: 'cover',
+//   overflow: 'hidden',
+//   //   zIndex: '-1',
+// });
+
+export const IPhoneBackgroundImage = styled.div({
+  position: 'relative',
+  height: '434px',
+  width: '210px',
+  overflow: 'hidden',
+  backgroundColor: 'blue',
+  borderRadius: '30px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundImage: "url('/src/assets/pexels-rodnae-productions-8217860.jpg')",
+  objectFit: 'cover',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
 });
