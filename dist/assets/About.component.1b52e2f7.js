738e12b95a971a23336c95b47462ad35ae206468
Marquis <whitley.marquis1@yahoo.com>
2023-03-07 23:33:16 -0600

    removed dist

--- dist/assets/About.component.1b52e2f7.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index.a07af031.js";const t=()=>o("div",{children:o("h1",{children:"Hello From About Section!"})});export{t as default};
--- dist/assets/Dashboard.component.833566c2.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index.a07af031.js";const a=()=>r("div",{children:"Hello From Dashboard!"});export{a as default};
